17db06c1be553411003d9e5e21a911407a1531ca
Vladyslav Chornyi <chornogovlada@gmail.com>
2023-04-21 14:29:50 +0300

    change little tunning default brazze for use home route

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -27,7 +27,11 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <x-button>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-button class="ml-3">
                     {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
